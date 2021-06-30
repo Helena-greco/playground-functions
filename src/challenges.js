9f11bb8d0587c7c3f24fe580b039e2f745a0064f
Helena <nenafgreco@hotmail.com>
2021-06-30 15:05:54 -0300

    Alterando as explicaçoes para maior compreensao

--- src/challenges.js
+++ src/challenges.js
@@ -14,17 +14,15 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-// Ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
 function splitSentence(string) {
-  let word = string.split(' ');
+  let word = string.split(' '); // .split foi utilizado para separar as string com espaço, como determinado entre parênteses.
   return word;
 }
 
 // Desafio 4
-// Ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/join
 function concatName(array) {
   let result = [array[array.length - 1], array[0]];
-  return result.join(', ');
+  return result.join(', '); // .join foi utilizado para juntar strings que foram selecionadas pelo seu índice.
 }
 
 // Desafio 5
@@ -37,9 +35,7 @@ function highestCount(numbers) {
 }
 
 // Desafio 7
-/* * O comando Math.abs foi utilizado para calcular a distância de um número pelo outro, sendo colocado entre parênteses. ** https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs */
-
-function catAndMouse(mouse, cat1, cat2) {
+function catAndMouse(mouse, cat1, cat2) { // O comando Math.abs foi utilizado nas linhas 43 e 44 para calcular a distância do gato para o rato, sendo colocado entre parênteses.
   let cat1Position = Math.abs(mouse - cat1);
   let cat2Position = Math.abs(mouse - cat2);
   if (cat1Position > cat2Position) {
@@ -69,11 +65,8 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-
-/* * o comando .replace foi utilizado para substituir o valor de uma variável por outro em uma nova variável, começando primeiro pelo valor inicial, separado por vírgula e após o valor substituído. O valor em questão encontra-se entre barras para separar o caractere da frase em si e utiliza-se o 'g' para uma pesquisa global no valor. ** Link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replace */
-
 function encode(stringLetter) {
-  let newPhrase = stringLetter.replace(/a/g, '1');
+  let newPhrase = stringLetter.replace(/a/g, '1'); // foi criada uma nova variável para receber o novo valor que vai ser substituído, foi utilizado o comando .replace nas linhas 72 a 76, e 80 a 84 com o objetivo de substituir as vogais, que foram separadas entre barras para separar o valor dos demais, o 'g' para direcionar uma pesquisa global, feita em toda a string, em números como solicitado, e vice-versa.
   newPhrase = newPhrase.replace(/e/g, '2');
   newPhrase = newPhrase.replace(/i/g, '3');
   newPhrase = newPhrase.replace(/o/g, '4');
