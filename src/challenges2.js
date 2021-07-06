cfbad7b02a8c1d6a6c1c93a0d0c86cd0a5515aca
Helena <nenafgreco@hotmail.com>
2021-07-06 09:46:02 -0300

    Desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -29,7 +29,7 @@ function generatePhoneNumber(telephoneNumber) {
   return createNumber(telephoneNumber);
 }
 
-function compareNumber(numbers) {
+function compareNumber(numbers) { // função para comparar as propriedades e retornar os valores
   for (let num of numbers) {
     if (num < 0 || num > 9 || counter(numbers, num) > 2) {
       return 'não é possível gerar um número de telefone com esses valores';
@@ -37,7 +37,7 @@ function compareNumber(numbers) {
   }
 }
 
-function counter(array, number) {
+function counter(array, number) { // função auxiliar para contabilizar os números repetidos do array
   let count = 0;
   for (let numero of array) {
     if (numero === number) {
@@ -47,7 +47,7 @@ function counter(array, number) {
   return count;
 }
 
-function createNumber(phoneNumber) {
+function createNumber(phoneNumber) { // função auxiliar para criar o número de telefone a partir do array determinado
   let num = ['('];
   for (let index = 0; index < phoneNumber.length; index += 1) {
     if (index === 2) {
@@ -71,13 +71,25 @@ function triangleCheck(lineA, lineB, lineC) {
     return false;
   } if (lineA < Math.abs(lineB - lineC)) {
     return false;
-  } 
+  }
   return true;
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(sugestion) {
+  let water = 0;
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // array para comparação
+  for (let caracter of sugestion) { // percorre a string
+    for (let index in numbers) { // percorre o array
+      if (caracter == numbers[index]) { // comparar o número que aparece na string sugestion com o array de números.
+        water += numbers[index];
+      }
+    }
+  }
+  if (water === 1) {
+    return water + ' copo de água';
+  }
+  return water + ' copos de água'; // retorna quando a quantidade de água for diferente de 1 copo.
 }
 
 module.exports = {
